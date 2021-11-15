d35fe3bbc5673db3cea07cb78d6bfa8868a6fbb7
Rahul Negi <54587454+RahulNegi123@users.noreply.github.com>
2021-11-15 22:12:06 +0530

    Update Q2.cpp

--- Week 6/Q2.cpp
+++ Week 6/Q2.cpp
@@ -1,80 +1,47 @@
-#include <bits/stdc++.h>
-void visited(int** , int, int);
-bool bipartite(int** ,int);
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> adj;
+vector<bool> vis;
+vector<int> col;
+bool bipart;
+void color(int u, int curr)
+{
+    if(col[u] != -1 and col[u]!= curr)
+    {
+        bipart = false;
+        return;
+    }
+    col[u]= curr;
+    if(vis[u])
+        return;
+    vis[u] = true;
+    for(auto i: adj[u])
+    {
+        color(i,curr xor 1);
+    }
+}
 int main()
 {
-	int n;		std::cin >> n;
-	int **arr;
-    arr = (int **)malloc(n * sizeof(int *));
-	for (int i = 0; i < n; i++)
-	{
-        arr[i] = (int *)malloc(n * sizeof(int));
-	}
-	for (int i = 0; i < n; i++)
-	{
-        for (int j = 0; j < n; j++)
-        {
-            std::cin >> arr[i][j];
-        }
-	}
-	std::cout<<std::endl;
-
-    int t;
-    int e;
-    std::cin >> t;
-    std::cin >> e;
-    bool Bipartite1 = bipartite(arr, n);
-    if (Bipartite1==true)
+    int n,m;
+    cin>>n>>m;
+    adj = vector<vector<int>>(n);
+    vis = vector<bool>(n,false);
+    col = vector<int>(n,-1);
+    bipart = true;
+    for(int i=0;i<m;i++)
     {
-        std::cout << "Yes Bipartite\n";
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
+    for(int i=0;i<n;i++)
+        if(!vis[i])
+            color(i,0);
+
+
+    if(bipart)
+        cout<<"Yes Bipartite";
     else
-    {	
-        std::cout << "Not Bipartite\n";
-    }
-
-	return 0;
-}
-void visited(int **A, int n, int j)
-{
-	for (int i = 0; i < n; i++)
-	{
-        if (A[i][j] == 1)
-        {
-            A[i][j] = 2;
-        }
-    }
-}
-bool bipartite(int **A, int n)
-{
-	std::queue<int> Queue;
-    Queue.push(0);
-    int adj[n] = {1};
-    visited(A, n, 0);
-    while (!Queue.empty())
-    {
-        int v = Queue.front();
-        Queue.pop();
-        int colnow = adj[v] * -1;
-        for (int i=0;i<n;i++)
-        {
-            if (A[v][i]!=0)
-            {
-                if (adj[i] == 0)
-                {
-                    adj[i] = colnow;
-                }
-                else if (adj[i] != colnow)
-                {
-                    return false;
-                }
-                if (A[v][i] == 1)
-                {
-                    Queue.push(i);
-                    visited(A, n, i);
-                }
-            }
-        }
-    }
-    return true;
+        cout<<"Not Bipartite"; return 0;
 }
